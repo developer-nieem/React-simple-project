1dc264613b0b75d0d0d7d7566c3c953c3583fcf0
developer-nieem <naimhossenwork@gmail.com>
2023-12-24 19:45:17 +0600

    complete header design

---
+++ src/common/shared/Header.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+
+const Header = () => {
+
+const menu = <>
+
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Projects</a></li>
+</>
+
+const [mNav , setMNav] =  useState(false)
+
+
+console.log(mNav);
+
+
+
+
+
+    return (
+        <header className="flex justify-between p-6 shadow-lg items-center">
+            <div>
+                <h2 className="text-xl font-semibold">BootCamp AI</h2>
+            </div>
+
+            {/* mobile menu */}
+            <div className="xl:hidden">
+                <button onClick={() => setMNav(!mNav)} className="xl:hidden bg-blue-600 px-3 text-xl text-white py-2 rounded-md hover:bg-blue-900"><HiOutlineMenuAlt3 /></button>
+
+                <ul className={` ${mNav ? "block" : "hidden"} absolute right-0  top-20 bg-slate-300  p-10 rounded-xl  shadow-xl `  }>
+                    {menu}
+                </ul>
+            </div>
+            {/* mobile menu end */}
+            <nav className="hidden xl:block">
+                <ul className="flex gap-4">
+                   {
+                    menu
+                   }
+                </ul>
+            </nav>
+
+            <div className="hidden xl:block">
+                <button className="bg-blue-600 px-3 text-xl text-white py-2 rounded-md hover:bg-blue-900">Subscribe</button>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
